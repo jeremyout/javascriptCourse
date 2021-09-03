28a5aa0dd5bccb74277a8f993a4f3a47c751ee01
chewy927 <jeremyoutinen@gmail.com>
2021-09-03 15:18:43 -0500

    Data types lesson and assignment
    1. Declare a variable called 'isIsland' and set its value according to your
    country. The variable should hold a Boolean value. Also declare a variable
    'language', but don't assign it any value yet
    2. Log the types of 'isIsland', 'population', 'country' and 'language'
    to the console

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,7 @@
+/*
+* Values and Variables lesson and assignment
+*/
+/* 
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -18,4 +22,42 @@ let population = 328;
 
 console.log('Country: ' + country);
 console.log('Continent: ' + continent);
-console.log('Population: ' + population + ' million')
+console.log('Population: ' + population + ' million');
+*/
+
+/*
+* Data types lesson and assignment
+*/
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jeremy');
+
+//dynamic typing example
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1990;
+console.log(year);
+console.log(typeof year);
+
+// Bug - shows up as an object
+console.log(typeof null);
+
+//new vars from this assignment
+let isIsland = false;
+let language;
+
+// repeat vars from the first assigment
+let population = 328;
+let country = 'United States';
+
+console.log('isIsland type: ' + typeof isIsland);
+console.log('language type: ' + typeof language);
