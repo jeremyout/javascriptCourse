0c332ae3e253c94b62d74d2a9ed7cf6f6efd466c
chewy927 <jeremyoutinen@gmail.com>
2021-09-03 13:16:12 -0500

    Values and Variables lesson
    Basic overview of values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,13 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
-
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jeremy');
+console.log(23);
+
+let firstName = 'Jeremy';
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob, myCurrentJob);
