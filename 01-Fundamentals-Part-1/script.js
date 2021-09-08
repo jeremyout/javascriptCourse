7c5469cf184b41baddd4ce0e703c98b3ff76d9ca
chewy927 <jeremyoutinen@gmail.com>
2021-09-08 13:05:34 -0500

    Truthy and falsy values

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -170,6 +170,7 @@ console.log(century);
 /*
 Type conversion and coercion
 */
+/*
 //  Type conversion
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
@@ -191,3 +192,31 @@ console.log('23' > '18');
 let n = '1' + 1; // '11'
 n = n - 1; // 11 - 1 (- operator converts '11' to number 11)
 console.log(n);
+*/
+
+/*
+Truthy and Falsy values
+*/
+
+// 5 falsy values: 0, '', undefined, null, and NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+
+if (money) {
+    console.log("Don't spend it all!");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+
+if (height) {
+    console.log('Yay, height is defined');
+} else {
+    console.log('height is undefined');
+}
