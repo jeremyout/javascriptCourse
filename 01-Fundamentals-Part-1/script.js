8539d0e7f493025a09f9cbabecd1b6fc362c8d7c
chewy927 <jeremyoutinen@gmail.com>
2021-09-08 13:26:06 -0500

    Equality operators lesson and assignment

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -197,7 +197,7 @@ console.log(n);
 /*
 Truthy and Falsy values
 */
-
+/*
 // 5 falsy values: 0, '', undefined, null, and NaN
 console.log(Boolean(0));
 console.log(Boolean(undefined));
@@ -219,4 +219,33 @@ if (height) {
     console.log('Yay, height is defined');
 } else {
     console.log('height is undefined');
+}
+*/
+
+/*
+Equality operators
+*/
+
+const age = '18';
+if (age === 18) console.log('You just became and adult (strict)');
+// == does type coercion, but === is strict equality. so '18' == 18 returns true, but '18' === 18 is false;
+if (age == 18) console.log('You just became and adult (loose)');
+
+const favorite = Number(prompt("What's your favorite number?"));
+
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+    console.log('Cool, 23 is an amazing number');
+} else if (favorite === 7) {
+    console.log('7 is also a cool number')
+} else if (favorite === 9) {
+    console.log('9 is also a cool number')
+} else {
+    console.log('Number is not 23, 7, or 9');
+}
+
+if (favorite !== 23) {
+    console.log('Why not 23?');
 }
