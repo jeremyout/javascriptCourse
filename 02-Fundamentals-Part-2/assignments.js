05d0f75c81443446476ec527f6c4b39f84931b5d
chewy927 <jeremyoutinen@gmail.com>
2021-10-25 09:54:24 -0500

    The while loop lesson and assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -286,3 +286,23 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
         console.log(`Neighbor(s) for country ${i + 1}: ${listOfNeighbours[i][y]}`);
     }
 }
+
+/*
+The while loop
+*/
+/*
+1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing',
+but this time using a while loop (call the array 'percentages3')
+2. Reflect on what solution you like better for this task: the for loop or the while
+loop?
+*/
+console.log(`--------- while loop ---------`)
+const percentages3 = [];
+let i = 0;
+while (i < populations.length) {
+    const perc = percentageOfWorld1(populations[i]);
+    percentages3.push(perc);
+    i++;
+}
+console.log(percentages3);
+// For this case, I prefer the for loop since you need to iterate on the index anyways
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -354,7 +354,7 @@ for (let i = 0; i < jonasArray.length; i++) {
 /*
 Looping backwards, and loops in loops
 */
-
+/*
 const jonasArray = [
     'Jonas',
     'Schmedtmann',
@@ -373,3 +373,27 @@ for (let exercise = 1; exercise <= 3; exercise++) {
         console.log(`Lifting weight repitition ${rep} 🏋️‍♂️`)
     }
 }
+*/
+
+/*
+The while loop
+*/
+
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repitition ${rep} 🏋️‍♂️`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+    console.log(`Lifting weights repitition ${rep} 🏋️‍♂️`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`Loop is about to end..`);
+}
+console.log(dice);
