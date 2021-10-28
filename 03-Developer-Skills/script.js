59af1c42dbca41f19d4e9d2f7313bc862b1510aa
chewy927 <jeremyoutinen@gmail.com>
2021-10-27 23:04:25 -0500

    Using Google, Stackoverflow, and MDN

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,9 +1,100 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = 23;
+/*
+const x = '23';
 if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2037 - birthYear;
 
-console.log();
+console.log(calcAge(1991));
+*/
+
+/*
+Using Google, Stackoverflow, and MDN
+*/
+/* 
+Problem 1:
+We work for a company building a smart home thermometer. Our most recent
+task is is: "Given an array of temperatures of a day, calculate the temperature
+amplitude. Keep in mind that sometimes there mightr be a sensor error."
+*/
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temperature amplitude? Answer: Difference between highest and lowest temp
+// - How to compute the max and min temperatures?
+// - What does a sensor error look like? What to do when an error occurs?
+
+// 2) Breaking up into sub-problems
+// - How to ignore sensor errors?
+// - Find max value in temperature array?
+// - Find min value in temperature array?
+// - Subtract min from max (amplitude) and return it
+
+function calcTempAmplitude(temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    // Ignore sensor errors
+    if (typeof temps[i] !== 'number') continue;
+
+    // Set the new value for the maximum if current index is greater than the current max
+    if (temps[i] > max) {
+      max = temps[i];
+    }
+
+    // Set the new value for the minimum if current index is greater than the current min
+    if (temps[i] < min) {
+      min = temps[i];
+    }
+  }
+
+  return max - min;
+}
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+/*
+Problem 2: 
+Function should now receive 2 arrays of temps
+*/
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? No!
+// just merge 2 arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2);
+
+function calcTempAmplitudeNew(t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    // Ignore sensor errors
+    if (typeof temps[i] !== 'number') continue;
+
+    // Set the new value for the maximum if current index is greater than the current max
+    if (temps[i] > max) {
+      max = temps[i];
+    }
+
+    // Set the new value for the minimum if current index is greater than the current min
+    if (temps[i] < min) {
+      min = temps[i];
+    }
+  }
+
+  return max - min;
+}
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
