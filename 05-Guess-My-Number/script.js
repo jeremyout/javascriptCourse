f3c7676583cfb4b0259d0e469df21572ab2dd810
chewy927 <jeremyoutinen@gmail.com>
2021-11-08 20:46:54 -0600

    Implementing the game logic
    Also set so that the number input cannot go negative by pressing the down button

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -11,12 +11,39 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
-  console.log(typeof guess);
+  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number Detected';
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.highscore').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lose';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lose';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
