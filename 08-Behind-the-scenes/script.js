59dc3f8a8debbdeb2e935bbef5b030aee2c561b3
chewy927 <jeremyoutinen@gmail.com>
2021-11-18 11:00:38 -0600

    Scoping in practice lesson

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating new variable with the same name as outer scopes variable
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      // re-defining outer scopes variable
+      output = 'new output!';
+    }
+    // console.log(str); // not in current scope
+    console.log(millenial);
+    // console.log(add(2, 3)); // not in current scope
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age); // not in current scope
+// printAge(); // not in current scope
