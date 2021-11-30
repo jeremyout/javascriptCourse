a4d378d37b0d00cc760620391b6153ba63e56df6
chewy927 <jeremyoutinen@gmail.com>
2021-11-29 18:29:22 -0600

    Hoisting and the TDZ in practice

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/*
+Scoping in practice
+*/
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -34,3 +38,54 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age); // not in current scope
 // printAge(); // not in current scope
+*/
+
+/*
+Hoisting and the TDZ in practice
+*/
+
+// Variables
+console.log(me); // outputs 'undefined'
+// console.log(job); // reference error
+// console.log(year); // reference error
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3)); // reference error
+console.log(addArrow);
+// console.log(addArrow(2, 3)); // reference error
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+// numProducs not set here yet, undefined due to hoisting
+// undefined is a falsy value
+if (!numProducts) {
+  deleteShoppingCart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log(`All products deleted`);
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
