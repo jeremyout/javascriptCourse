61c739c308a6a29c99f0b9945ddb624e6dc0973d
chewy927 <jeremyoutinen@gmail.com>
2021-11-29 19:33:48 -0600

    The this keyword in practice

--- 08-Behind-the-scenes/script.js
+++ 08-Behind-the-scenes/script.js
@@ -43,7 +43,7 @@ calcAge(1991);
 /*
 Hoisting and the TDZ in practice
 */
-
+/*
 // Variables
 console.log(me); // outputs 'undefined'
 // console.log(job); // reference error
@@ -89,3 +89,42 @@ const z = 3;
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+/*
+The this keyword in practice
+*/
+
+// console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  // console.log(this);
+};
+calcAgeArrow(1991);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+// f(); // 'this' keyword is undefined here, similar to calcAge call on line 104 above
