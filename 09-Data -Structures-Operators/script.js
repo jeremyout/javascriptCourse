9abefc26382884142fbeec130cbc57ef4ca9ca93
chewy927 <jeremyoutinen@gmail.com>
2021-11-30 16:23:33 -0600

    Destructuring Arrays

--- 09-Data -Structures-Operators/script.js
+++ 09-Data -Structures-Operators/script.js
@@ -26,4 +26,50 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+/*
+Destructuring Arrays
+*/
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// Split the array into individual parameters
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+// Only take specific elements out of the categories array
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// How to swap without destructuring (need a temp variable)
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// Swapping with destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function and put into their own variables
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8];
+console.log(p, q, r);
