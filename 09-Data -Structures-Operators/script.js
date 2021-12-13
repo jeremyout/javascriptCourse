33a28b9e8643e1b06a523af3ab17737a38d42634
chewy927 <jeremyoutinen@gmail.com>
2021-12-13 17:20:04 -0600

    Rest pattern and parameters

--- 09-Data -Structures-Operators/script.js
+++ 09-Data -Structures-Operators/script.js
@@ -44,6 +44,10 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 /*
@@ -146,7 +150,7 @@ restaurant.orderDelivery({
 /*
 The spread operator
 */
-
+/*
 const arr = [7, 8, 9];
 const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArray);
@@ -208,3 +212,48 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristoraunte Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+*/
+
+/*
+Rest pattern and parameters
+*/
+
+// 1) Destructuring
+
+// Spread because used on the right side of the equal sign
+const arr = [1, 2, ...[3, 4]];
+
+// Rest because used on the left side of the equal sign
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+// The Rest operator always has to be last in the assignment
+// There can only be one Rest in any destructuring assignment
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Rest with Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2.) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  console.log(numbers);
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
