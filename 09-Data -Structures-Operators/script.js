1bb5157b24a54e6c97d24b51a8474835199568cc
chewy927 <jeremyoutinen@gmail.com>
2021-12-29 10:32:25 -0600

    Looping arrays, the for-of loop

--- 09-Data -Structures-Operators/script.js
+++ 09-Data -Structures-Operators/script.js
@@ -319,7 +319,7 @@ console.log(guestCorrect);
 /*
 Logical assignment operators
 */
-
+/*
 const rest1 = {
   name: 'Capri',
   numGuests: 0,
@@ -359,3 +359,19 @@ rest2.owner &&= '<ANONYMOUS>';
 
 console.log(rest1);
 console.log(rest2);
+*/
+
+/*
+Looping arrays: The for-of loop
+*/
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// console.log([...menu.entries()]);
