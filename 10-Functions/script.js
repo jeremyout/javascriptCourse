096350b984889f4590e21f8958f1f540c9fdfe11
chewy927 <jeremyoutinen@gmail.com>
2022-01-20 11:17:36 -0600

    Default parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+/*
+Default parameters
+*/
+
+const bookings = [];
+
+//ES6 can specify default parameters in function definition
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5 way of implementing default parameters
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+// How to skip a parameter that we want to leave as default
+createBooking('LH123', undefined, 1000);
