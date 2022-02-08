060b80d160644684388dbc497cecce724950dd75
chewy927 <jeremyoutinen@gmail.com>
2022-02-07 22:14:06 -0600

    The map method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -86,7 +86,7 @@ displayMovements(account1.movements);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -265,7 +265,7 @@ Creating DOM elements
 /*
 Data transformations: Map, filter, reduce
 */
-
+/*
 // The map method is yet another method that we can use to loop over arrays.
 // - Map is similar to the forEach method but Map creates a
 //   brand new array containing the results of applying an operation
@@ -288,3 +288,35 @@ console.log(arr, filter1);
 
 const reduce1 = arr.reduce((previous, current) => previous + current);
 console.log(arr, reduce1);
+*/
+
+/*
+The map method
+*/
+
+// The map method does not mutate the original method
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+
+// Arrow function version
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (let mov of movements) {
+  movementsUSDfor.push(mov * eurToUsd);
+}
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(function (mov, i) {
+  return `Movement ${
+    i + 1
+  }: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`;
+});
+console.log(movementsDescriptions);
