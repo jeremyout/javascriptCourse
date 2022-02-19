55a8bca73df6b2ea036c27d456611a30159bcec1
chewy927 <jeremyoutinen@gmail.com>
2022-02-19 10:29:37 -0600

    Coding challenge 2 - updates from solution

--- 11-Arrays-Bankist/codingChallenge2.js
+++ 11-Arrays-Bankist/codingChallenge2.js
@@ -23,26 +23,25 @@ Test data:
 GOOD LUCK 😀
 */
 
-const humanAge = [];
 const calcAverageHumanAge = function (ages) {
-  ages.forEach(function (age) {
-    if (age <= 2) {
-      humanAge.push(2 * age);
-    } else if (age > 2) {
-      humanAge.push(16 + age * 4);
-    }
-  });
+  const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   console.log(humanAge);
-  const adultDogs = humanAge.filter(function (age) {
-    return age >= 18;
-  });
+  const adultDogs = humanAge.filter(age => age >= 18);
   console.log(adultDogs);
   const averageAge =
     adultDogs.reduce(function (acc, curr) {
       return acc + curr;
-    }) / adultDogs.length;
-  console.log(averageAge);
+    }, 0) / adultDogs.length;
+  return averageAge;
 };
 
-// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+
+// Could also calculate the average in the following way
+// const adultDogs = [36, 32, 76, 48, 28];
+// const averageAge = adultDogs.reduce(function (acc, age, i, arr) {
+//   return acc + age / arr.length;
+// }, 0);
+// console.log(averageAge);
