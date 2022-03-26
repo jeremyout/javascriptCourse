e87a6b4dd2abb97d4d114a36273180a871873d99
jeremyoutinen <jeremyoutinen@gmail.com>
2022-03-26 13:16:24 -0500

    Stlyes, classes, and attributes

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -106,3 +106,63 @@ document
 // moving up and down in the DOM tree is called DOM traversing
 
 // Review the insertAdjacentHTML that we used before in the bakist app
+
+/*
+Styles, attributes, and classes
+*/
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+// These styles are set as inline styles (directly in the DOM)
+
+// This only works for inline styles that we set ourselves
+// using the style property
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+// this returns a huge object
+console.log(getComputedStyle(message));
+// Can filter it down to a specific style
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// Setting custom properties - need to use the setProperty method
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src); // returns ths absolute URL
+console.log(logo.getAttribute('src')); // gets the relative URL
+console.log(logo.className);
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+// There is also a setAttribute method
+logo.setAttribute('company', 'Bankist');
+
+// Can also set the attributes
+logo.alt = 'Beautiful minimalist logo';
+console.log(logo.alt);
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // absolute link
+console.log(link.getAttribute('href')); // relative link
+
+// Data attributes
+console.log(logo.dataset.versionNumber); // camel case here, dash in index.html
+
+// Classes
+logo.classList.add('c', 'j'); // can pass in multiple values
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes like it is in arrays
+
+// Can use className to set the class, but shouldn't
+// DON'T USE THIS! - WILL OVERRIDE ALL THE CLASSES AND
+// ONLY ALLOWS US TO PUT ONE CLASS ON THE ELEMENT
+logo.className = 'jonas';
