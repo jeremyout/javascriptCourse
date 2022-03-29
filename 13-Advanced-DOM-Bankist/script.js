dff50aa3bf2072ad8dbf38f452f07da1258b9659
jeremyoutinen <jeremyoutinen@gmail.com>
2022-03-29 10:16:14 -0500

    Lazy Loading Images

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -241,7 +241,7 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   // guard clause
   if (!entry.isIntersecting) return;
   // Remove the hidden class when threshold is past
@@ -258,6 +258,31 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+///////////////////////////////////////
+// Lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+// console.log(imgTargets);
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) return;
+  // Replace the src attribute with data-src
+  entry.target.src = entry.target.dataset.src;
+  // wait for full-res image to load before removing the blur
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  // remove the observer for the entry that was just acted on
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ///////////////////////////////////////
 ///////////////////////////////////////
