d6184d22b1b4e2f31a19698e2772b543719ea3da
jeremyoutinen <jeremyoutinen@gmail.com>
2022-04-10 14:29:22 -0500

    Setters and Getters

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -240,8 +240,8 @@ ES6 Classes
 
 // Class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
   // Methods will be added to the .prototype property
@@ -250,13 +250,32 @@ class PersonCl {
   }
 
   greet() {
-    console.log(`Hey ${this.firstName}`);
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name`);
+    }
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 console.log(Object.getPrototypeOf(jessica) === PersonCl.prototype);
 
 // Can also add a method manually to the prototype
@@ -276,3 +295,25 @@ jessica.greet();
 // Constructor functions are not old/deprecated syntax, fine to keep using them (Personal preference)
 
 // Some people say that classes are really bad and shouldn't be used.
+
+/*
+Setters and Getters
+*/
+
+const walter = new PersonCl('Walter White', 1965);
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+
+account.latest = 50;
