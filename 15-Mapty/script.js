2145e7980917f78a45789794c625e2aef7d5cab7
jeremyoutinen <jeremyoutinen@gmail.com>
2022-04-17 16:29:38 -0500

    Managing Workout Data: Creating Classes

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -79,6 +79,53 @@ Project planning
 Using the Geolocation API
 */
 
+class Workout {
+  // Keep in mind that adding these properties are not yet part of ES6
+  date = new Date();
+  // Usually we don't create IDs on our own and instead use a library to do it
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat,lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // In min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/hr
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
+/////////////////////////////////////////////////
+// Application architecture
 class App {
   #map;
   #mapEvent;
