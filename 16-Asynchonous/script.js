6b83b35c6193589fc8ce0d0981ee127284568614
jeremyoutinen <jeremyoutinen@gmail.com>
2022-04-24 15:50:08 -0500

    Important: API URL Change

--- 16-Asynchonous/script.js
+++ 16-Asynchonous/script.js
@@ -59,3 +59,24 @@ Asynchronous JavaScript, AJAX, and APIs
 
 // These days most APIs use JSON data
 // JSON is the most popular API data format
+
+/*
+Important: API URL Change
+*/
+
+/*
+🚨 The base URL of the API used throughout this section has changed
+
+It's not a big deal, it's really just one small change. Instead of:
+
+https://restcountries.eu/rest/v2/
+
+It's now:
+
+https://restcountries.com/v2/
+
+So whenever we use the Countries API in this section, please just change the URL you see 
+in the videos to this new one.
+
+Happy learning ✌️
+*/
