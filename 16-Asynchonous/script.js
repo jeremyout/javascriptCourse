7bbb10c01238c168d118d3f0889c4d375ef30c0d
jeremyoutinen <jeremyoutinen@gmail.com>
2022-04-24 17:31:30 -0500

    Welcome to callback hell

--- 16-Asynchonous/script.js
+++ 16-Asynchonous/script.js
@@ -85,7 +85,7 @@ const countriesContainer = document.querySelector('.countries');
 /*
 Our first AJAX Call: XMLHttpRequest
 */
-
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -117,7 +117,7 @@ const getCountryData = function (country) {
 getCountryData('portugal');
 getCountryData('usa');
 getCountryData('germany');
-
+*/
 /*
 How the web works: Requests and Responses
 */
@@ -222,3 +222,89 @@ How the web works: Requests and Responses
 
 // As a second part, the job of the IP protocol is to send and route these packets through the internet. So
 // it ensures that they arrive at the destination they should go using IP addresses on each packet.
+
+/*
+Welcome to callback hell
+*/
+
+// In the last lecture we did a simple AJAX call to fetch data from a countries API
+// So we created a function for that and as we called the function multiple times, multiple AJAX
+// calls were made at the same time. They were basically running in parallel and we couldnn't control
+// which one finished first.
+
+// To accomplish this we need to implement a sequence of AJAX calls
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = function (country) {
+  // AJAX Call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // Render country 1
+    renderCountry(data);
+    // Get neighbor country (2)
+    const [neighbor] = data.borders;
+    if (!neighbor) return;
+
+    // AJAX Call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbor}`);
+    request2.send();
+
+    // Now we have another callback inside of a callback
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      // Render country 2
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbor('portugal');
+getCountryAndNeighbor('usa');
+
+// Callback hell is when we have a lot of nested callbacks in order to execute asynchronous tasks in sequence
+// This happens for all asynchronous tasks which are handled by callbacks, not just AJAX
+
+// Example of callback hell
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+// The problem with callback hell is that it makes our code look very messy. Even more important
+// it makes our code hard to maintain and difficult to understand and to reason about. Code that is
+// hard to understand and difficult to reason will have more bugs and is just worse code.
+
+// Fortunately for us, there is a way to escape callback hell and that is by using promises
