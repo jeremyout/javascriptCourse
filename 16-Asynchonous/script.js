8226bcc2ce0fb976acefd29149e0667df4997497
jeremyoutinen <jeremyoutinen@gmail.com>
2022-04-24 20:48:09 -0500

    Consuming promises

--- 16-Asynchonous/script.js
+++ 16-Asynchonous/script.js
@@ -313,7 +313,7 @@ setTimeout(() => {
 /*
 Promises and the Fetch API
 */
-
+/*
 // const request = new XMLHttpRequest();
 // request.open('GET', `https://restcountries.com/v2/name/${country}`);
 // request.send();
@@ -366,3 +366,49 @@ console.log(request);
 
 // Most of the time we will just consume promises which is the easier and more useful part.
 // But sometimes we need to build a promise and not just consume it.
+*/
+
+/*
+Consuming promises
+*/
+// const request = fetch(`https://restcountries.com/v2/name/usa`);
+// console.log(request);
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json(); // This returns another promise, so we need to handle that with another .then call
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+// Simplified with arrow functions
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('portugal');
