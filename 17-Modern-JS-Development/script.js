3a518fc4c0a1039c74a6ac2da7caafbf1c4e6474
jeremyoutinen <jeremyoutinen@gmail.com>
2022-05-05 17:07:32 -0500

    An overview of modules in JS

--- 17-Modern-JS-Development/script.js
+++ 17-Modern-JS-Development/script.js
@@ -44,3 +44,96 @@ An Overview of Modern JS Development
 // Webpack and Parcel are also available on npm
 // npm contains development tools that help build our applications as
 // well (Live-Server, Parcel, Babel, etc.)
+
+/*
+An overview of modules in JS
+*/
+
+// A module is a reusable piece of code that encapsulates
+// implementation details of a certain part of our project
+// Usually a standalone file but doesn't have to be
+// A module normally contains some code, but it can also
+// include imports and exports
+
+// With exports, we can export values out of a module
+// For example, simple values or even functions
+// Whatever we export from a module is called the public API
+// This is just like classes where we can also expose a public API
+// for other code to consume.
+
+// In the case of modules, this public API is consumed by importing
+// values into a module. Just like we can export, in modules, we can
+// usually also import values from other modules. The imported modules
+// are then called dependencies of the importing module. The code that
+// is in the module that is importing cannot work without the code that
+// it is importing from the external module.
+
+// Why modules?
+// - Modules make it really easy to compose software. Modules are small
+//   building blocks that we put together to make complex applications
+// - Isolate components: Modules can be developed in isolation without
+//   thinkking about the entire codebase
+// - Abstract code: Implement low-level code in modules and import these
+//   abstractions into other modules.
+// - Organized code: Modules naturally lead to a more organized codebase
+// - Reuse code: Modules allow us to easily reuse the same code, even
+//   across multiple projects.
+
+// Native JS (ES6) Modules
+// ES6 Modules are modules that are actually stored in files, exactly
+// one module per file.
+
+// ES6 Modules vs Scripts
+// In modules, all top level variables are scoped to the module. So
+// varaibles are private to the module by default. The only way an
+// outside module can access a value that's inside another module is
+// by exporting that value. If we don't export then nobody else can see
+// the variable.
+// In scripts, all top-level variables are always public. This can lead
+// to problems like global namespace pollution.
+// ES6 modules are always executed in strict mode, while scripts on the
+// other hand are executed in sloppy mode by default. So with modules,
+// there's no more need to declare strict mode.
+// The this keyword is always undefined in ES6 Modules while in scripts it
+// points at the window object.
+// ES6 Modules support imports and exports, while that isn't possible with
+// scripts.
+// Imports and exports can only happen at the top-level, outside functons
+// and if blocks.
+// All imports are hoisted, so it's like they are defined at the top of the
+// file. In practice, importing values is always the first thing done in
+// a module.
+// In order to link a module to an HTML file, we need to use the
+// script tage with the type attribute set to module instead of
+// just the plain script tag.
+// Downloading module files always happens asynchronously. This is
+// true for a module loaded from HTML as well as for modules that are
+// loaded by importing one module into another using the import syntax
+// Regular scripts are downloaded by default in a blocking synchronous
+// way unless we use the async or defer attributes on the script tag
+
+// How modules import other modules behind the scenes
+// The first step is to parse the code. Parsing means to just read it
+// without executing. This is when imports are executed. The whole process
+// of importing modules happens before the code in the main modules is
+// actually executed.
+// Modules are imported synchronously.
+// This is only possible thanks to top-level imports which make imports
+// known before execution.
+// Modules are imported synchronously to make bundling and dead code
+// elimination possible.
+
+// After the parsing is complete and figured out the modules it needs
+// then the modules are downloaded from the server. Downloading happens
+// asynchronously. It's only the importing operation that happens
+// synchronously.
+// After a module arrives, it's parsed and then the modules exports are
+// linked to the imports.
+// Imported values are a live connection, not a copy!!
+// When a value changes in the exporting module, then the value also
+// changes in the importing module. This is unique to ES6 modules. Other
+// module systems do not work like this.
+
+// Next up, The code in the imported module is executed. And with this,
+// the process of importing modules is finished. Then it is finally time
+// for the importing module to be executed.
