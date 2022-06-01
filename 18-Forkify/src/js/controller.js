ef10086d3f5c4a3990a429a70ed952107e1aceca
jeremyoutinen <jeremyoutinen@gmail.com>
2022-05-31 22:55:21 -0500

    Loading a recipe from the API

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bccbd'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    // Throw error if bad response
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    // Reformat the data
+    // let recipe = data.data.recipe; // recipe on both sides, destructure it
+    let { recipe } = data.data;
+    // Redefine the recipe object with new property names. Other ways to do this,
+    // but this is the quick and dirty way.
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+showRecipe();
