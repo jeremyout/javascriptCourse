a5523e2a035b7e3fc63e32f9976224390a47202e
jeremyoutinen <jeremyoutinen@gmail.com>
2022-08-05 21:42:52 -0500

    Developing a DOM updating algorithm

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -20,6 +20,9 @@ const controlRecipes = async function () {
 
     recipeView.renderSpinner();
 
+    // 0.) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1.) Loading Recipe
     await model.loadRecipe(id);
 
@@ -85,7 +88,8 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
