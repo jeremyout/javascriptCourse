2b4b5ba3f7148a9c8cc72c224c8272c746535c3f
jeremyoutinen <jeremyoutinen@gmail.com>
2022-08-07 11:12:38 -0500

    Uploading a new recipe - part 1

---
+++ 18-Forkify/src/js/views/addRecipeView.js
@@ -0,0 +1,49 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
+
+  // Split out into its own function because the this keyword inside of the
+  // event listener would be pointing to _btnOpen, _btnClose, or _overlay
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      // this keyword here is pointing at the parent element, the upload form
+      // have to spread the return because it isn't usable otherwise
+      const dataArray = [...new FormData(this)];
+      // Take an array of entries and convert to an Object
+      const data = Object.fromEntries(dataArray);
+      // pass the data to the handler
+      handler(data);
+    });
+  }
+
+  _generateMarkup() {}
+}
+
+export default new AddRecipeView();
