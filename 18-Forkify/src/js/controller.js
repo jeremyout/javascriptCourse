e27b7c68700dcdf85886db0cb107bb4780dce7ce
jeremyoutinen <jeremyoutinen@gmail.com>
2022-08-06 23:31:41 -0500

    Storing bookmarks with localstorage

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -24,15 +24,17 @@ const controlRecipes = async function () {
 
     // 0.) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    // 1.) Updating bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    // 1.) Loading Recipe
+    // 2.) Loading Recipe
     await model.loadRecipe(id);
 
-    // 2.) Rendering recipe
+    // 3.) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 // controlRecipes ();
@@ -107,7 +109,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
--- 18-Forkify/src/js/model.js
+++ 18-Forkify/src/js/model.js
@@ -81,12 +81,19 @@ export const updateServings = function (newServings) {
   state.recipe.servings = newServings;
 };
 
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
 export const addBookmark = function (recipe) {
   // Add bookmark
   state.bookmarks.push(recipe);
 
   // Mark current recipe as bookmark
   if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  // Update localstorage
+  persistBookmarks();
 };
 
 // It's common in programming when you add something you provide the entire
@@ -97,4 +104,18 @@ export const deleteBookmark = function (id) {
   state.bookmarks.splice(index, 1);
   // Mark current recipe as NOT bookmarked
   if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+  // Update localstorage
+  persistBookmarks();
 };
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
+// clearBookmarks();
